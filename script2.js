9de5e422451a4f36c29d0f46e8407786f5b9bd8c
Muhammedkaif_28 <kaif.saiyed2003@gmail.com>
2025-08-15 14:32:06 +0530

    Add main HTML, CSS, and image folders

---
+++ script2.js
@@ -0,0 +1,10 @@
+function showDetails(title, image, description) {
+  document.getElementById('popup-title').innerText = title;
+  document.getElementById('popup-img').src = image;
+  document.getElementById('popup-desc').innerText = description;
+  document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
